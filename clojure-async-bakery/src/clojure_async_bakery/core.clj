481bf9c7bb04dfcb653f1daff8eda11338a418b4
john9hoff <hoffx247@morris.umn.edu>
2018-05-03 13:51:16 -0500

    # Please enter the commit message for your changes. Lines starting

--- clojure-async-bakery/src/clojure_async_bakery/core.clj
+++ clojure-async-bakery/src/clojure_async_bakery/core.clj
@@ -2,22 +2,37 @@
   (require [clojure.core.async :as async])
   (:gen-class))
 
-(def cust-line (async/chan))
-(def worker-line (async/chan))
-(def pairs (async/chan))
-(def results (async/chan))
+;(def pairs (async/chan))
+;(def results (async/chan))
 
 (defn initialize-universe
   [num-cust num-workers]
-  ; put all workers in worker channel right away
-  (async/go
-    (map (partial async/>! worker-line) (range 0 10))
-    (println "HELLO"))
-    (println "outside")
+  (let [worker-line (async/chan) customer-line (async/chan)]
+    ; put all workers in worker channel right away
+    (async/go
+      (doseq [w (range num-workers)]
+        (async/>! worker-line w)))
+    (async/go
+      (doseq [c (range num-cust)]
+        (async/>! customer-line)))
+  [customer-line worker-line])
   ; put all customers in customer channel after sleeping
 )
 
+(defn start
+  [customer-line worker-line num-cust]
+  (async/go
+    (doseq [c (range num-cust)]
+      (async/>! pairs
+        [(async/<! customer-line) (async/<! worker-line)])))
+; unfinished below
+  (async/go
+    (doseq [c (range num-cust)]
+      (async/>! pairs
+        [(async/<! customer-line) (async/<! worker-line)])))
+
 (defn -main
   [num-cust num-workers]
-  (initialize-universe num-cust num-workers)
-  (println "Main"))
+  (let [[customer-line worker-line]
+        (initialize-universe num-cust num-workers)]
+        (start customer-line worker-line num-cust)))
