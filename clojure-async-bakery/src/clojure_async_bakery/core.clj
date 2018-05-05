a523e3008395fbaec4090f8293f70351b7469031
Avery Koranda <koran023@morris.umn.edu>
2018-05-05 17:27:30 -0500

    Worked on pipeline in start function and added print messages

--- clojure-async-bakery/src/clojure_async_bakery/core.clj
+++ clojure-async-bakery/src/clojure_async_bakery/core.clj
@@ -5,31 +5,48 @@
 ;(def pairs (async/chan))
 ;(def results (async/chan))
 
+; got this function from https://stackoverflow.com/questions/8939970/a-recursive-fibonacci-function-in-clojure
+(defn fib [n]
+  (take n
+    (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1]))))
+
 (defn initialize-universe
   [num-cust num-workers]
   (let [worker-line (async/chan) customer-line (async/chan)]
     ; put all workers in worker channel right away
     (async/go
       (doseq [w (range num-workers)]
-        (async/>! worker-line w)))
+        (async/>! worker-line w)
+        (println "worker got in line")))
     (async/go
       (doseq [c (range num-cust)]
-        (async/>! customer-line)))
+        (async/>! customer-line [c (rand-int 43)])
+        (println "customer got in line")))
   [customer-line worker-line])
   ; put all customers in customer channel after sleeping
 )
 
 (defn start
   [customer-line worker-line num-cust]
-  (async/go
-    (doseq [c (range num-cust)]
-      (async/>! pairs
-        [(async/<! customer-line) (async/<! worker-line)])))
+  (let [pairs (async/chan) results (async/chan)]
+    (async/go
+      (doseq [c (range num-cust)]
+        (async/>! pairs
+          [(async/<!! customer-line) (async/<!! worker-line)])
+        (println "paired")))
+    (async/pipeline
+      10
+      results
+      (fn [x] (println x) x)
+      #_(fn
+        [x]
+        (fib (second (first x))))
+      pairs)))
 ; unfinished below
-  (async/go
-    (doseq [c (range num-cust)]
-      (async/>! pairs
-        [(async/<! customer-line) (async/<! worker-line)])))
+  ; (async/go
+  ;   (doseq [c (range num-cust)]
+  ;     (async/>! pairs
+  ;       [(async/<! customer-line) (async/<! worker-line)])))
 
 (defn -main
   [num-cust num-workers]
